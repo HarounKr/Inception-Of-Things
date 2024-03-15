f2539a42a902ba0fc393aaa17e407959e3d12299
Your Name <you@example.com>
2024-03-15 14:31:13 +0100

    P1 & P2 done

---
+++ P1/run.sh
@@ -0,0 +1,3 @@
+mkdir shared
+
+vagrant up
---
+++ P2/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+entries=(
+    "192.168.56.110 app1.com"
+    "192.168.56.110 app2.com"
+    "192.168.56.110 app3.com"
+)
+
+# Boucle à travers les entrées
+for entry in "${entries[@]}"; do
+    # Recherche si l'entrée existe déjà dans /etc/hosts
+    if ! grep -q "$entry" /etc/hosts; then
+        # Si l'entrée n'existe pas, l'ajoute au fichier
+        echo "$entry" | sudo tee -a /etc/hosts > /dev/null
+        echo "Added: $entry"
+    else
+        # Si l'entrée existe déjà, affiche un message
+        echo "Line exist: $entry"
+    fi
+done
+
+
+vagrant up
