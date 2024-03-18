69ece9b470d6794828847ea405a66bbd9ff0b7db
Your Name <you@example.com>
2024-03-18 16:42:35 +0100

    P3 done

---
+++ P3/run.sh
@@ -0,0 +1,17 @@
+sudo k3d cluster create P3cluster
+
+mkdir ~/.kube
+sudo k3d kubeconfig get P3cluster  > ~/.kube/config
+echo "export KUBECONFIG=~/.kube/config" > ~/.profile
+source ~/.profile
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+
+argocd admin initial-password -n argocd > argopass
+kubectl create namespace dev
+argocd proj create development -d https://kubernetes.default.svc, dev -s '*'
+kubectl apply -f ./conf/argocd.yaml
+sleep 2
+kubectl port-forward service/wil-service 8888:80 -n dev
